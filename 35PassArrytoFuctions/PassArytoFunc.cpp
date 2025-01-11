913c774d9e89948d1f17a924c6bedd74ae010fdb
Oppie1 <atindall729@gmail.com>
2025-01-10 18:27:07 -0600

    Add files via upload

---
+++ 35PassArrytoFuctions/PassArytoFunc.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int theArray[], int sizeOfArray);
+
+int main() {
+	int bucky[3] = { 20,54,675 };
+	int jessica[6] = { 54,24,7,8,9,99 };
+
+	printArray(jessica, 6);
+}
+void printArray(int theArray[],int sizeOfArray) {
+	for (int x = 0; x < sizeOfArray; x++) {
+		cout << theArray[x] << endl;
+}
+
+}
